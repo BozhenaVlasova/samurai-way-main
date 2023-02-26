8c597b8d5b2cdc24fda43f88eab19da542ec05f0
BozhenaVlasova <dix20sept@gmail.com>
2023-02-26 22:43:05 +0300

    removed function to re-call App

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store, {AppStateType} from "./Redux/redux-store";
+import store from "./Redux/redux-store";
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 
-const rerenderEntireTree = (state: AppStateType) => {
-    ReactDOM.render(
-        <BrowserRouter>
-            <Provider store={store}>
-            <App />
-            </Provider>
-        </BrowserRouter>,
-        document.getElementById('root')
-    );
-}
-rerenderEntireTree(store.getState())
-store.subscribe(() => {rerenderEntireTree(store.getState())});
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </BrowserRouter>,
+    document.getElementById('root')
+);

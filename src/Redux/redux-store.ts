9a36f09dbb7e2b74ec3db2eba202098f35db32c6
BozhenaVlasova <dix20sept@gmail.com>
2023-04-06 22:31:25 +0300

    added Login Form

--- src/Redux/redux-store.ts
+++ src/Redux/redux-store.ts
@@ -4,6 +4,7 @@ import dialogsReducer from "./dialogs-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
+import {reducer as formReducer} from "redux-form";
 
 export type AppStateType = ReturnType<typeof rootReducer>
 
@@ -11,7 +12,8 @@ let rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
 
 let store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
